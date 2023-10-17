740e75157a12fb433ae3648581ba4e9b6aadfcd4
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-17 14:21:03 +0800

    deploy: django allow all host

--- telethu/settings.py
+++ telethu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7!-l)z_f$1p(3y+afvs5p(r!2y*b_6t+n2=2hhj5#mr7re6fps
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
