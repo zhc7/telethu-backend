ffa9ef39de36c3b57794517fe9a78e4da0dea05e
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-19 16:12:33 +0800

    deploy: start rabbitmq in start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ echo "preparing server"
 python3 manage.py makemigrations
 echo "migrating database"
 python3 manage.py migrate
+rabbitmq-server -detached
 echo "starting server"
 python3 manage.py runserver 0.0.0.0:80
