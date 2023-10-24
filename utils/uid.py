512db7739c8f699cb84ae64996ad08e8d5fc1a51
zhengry22 <zhengry22@mails.tsinghua.edu.cn>
2023-10-24 21:26:36 +0800

    feat(uid): add MessageIdmaker

--- utils/uid.py
+++ utils/uid.py
@@ -1,5 +1,5 @@
 from threading import Lock
-from users.models import User, GroupList
+from users.models import User, GroupList, MessageList
 
 
 class IdMaker:
@@ -22,3 +22,18 @@ class IdMaker:
 
 # 使用globalIdMaker进行全局id生成，控制id的唯一性
 globalIdMaker = IdMaker()
+
+# TODO: 为所有的消息做一个上面的 id_maker
+class MessageIdMaker:
+    def __init__(self):
+        self.lock = Lock()
+        # 查找当前数据库中最大的 id，包括group_id和user_id
+        max_id = max(MessageList.objects.all().values_list('id', flat=True))
+        if max_id is None:
+            max_id = 0
+        self.id = max_id
+
+    def get_id(self):
+        with self.lock:
+            self.id += 1
+        return self.id
