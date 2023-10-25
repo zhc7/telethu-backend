31bae91bc62d768c69d5f9628f189d6a0a1568d9
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-25 10:07:38 +0800

    refactor: move data types to independent file

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,61 +1,12 @@
-import enum
 import json
 
 import aio_pika
 from channels.db import database_sync_to_async  # 引入异步数据库操作
 from channels.generic.websocket import AsyncWebsocketConsumer
-from pydantic import BaseModel
 
-from users.models import Friendship, GroupList, User, MessageList
+from users.models import Friendship, GroupList, User
+from utils.data import MessageType, TargetType, Message, ContactsData, UserData, GroupData
 from utils.uid import globalIdMaker, globalMessageIdMaker
-
-
-class MessageType(enum.IntEnum):
-    TEXT = 0
-    IMAGE = 1
-    AUDIO = 2
-    VIDEO = 3
-    FILE = 4
-    FUNCTION = (
-        5  # this marks the line between message and function, do not set this directly
-    )
-    FUNC_ADD_FRIEND = 6
-    FUNC_CREATE_GROUP = 7
-    FUNC_ADD_GROUP_MEMBER = 8
-
-
-class TargetType(enum.IntEnum):
-    FRIEND = 0
-    GROUP = 1
-    OTHER = 2
-
-
-class Message(BaseModel):
-    message_id: int
-    m_type: MessageType = MessageType.TEXT
-    t_type: TargetType = TargetType.OTHER
-    time: float
-    content: str | list | int  # 如果是消息，content 是 str，如果是函数，content 是 list,如果是群加人，这个放群id
-    sender: int  # 如果是消息，sender 是发送者的 id，如果是函数，sender 是函数的发起者的 id。如果是群加人，这个放拉人的人
-    receiver: int  # 如果是消息，receiver 是接收者的 id，如果是函数，receiver 是函数的接收者的 id。如果是群加人，这个放被拉的人
-    info: str  # for message referencing, forwarding and appending info
-
-
-class ContactsData(BaseModel):
-    id: int
-    name: str
-    avatar: str
-    category: str
-
-
-class UserData(ContactsData):
-    email: str
-    category: str = "user"
-
-
-class GroupData(ContactsData):
-    members: list[UserData]
-    category: str = "group"
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -262,7 +213,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                         await self.chat_message(message)
 
     async def send_package_direct(
-        self, message: Message, receiver: str
+            self, message: Message, receiver: str
     ):  # 无论是什么，总会将一个package发送进direct queue
         message_json = message.model_dump_json()
         # 发送消息给rabbitmq
