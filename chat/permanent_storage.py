3e8f905380ee0d596d66a80dc605bad950a435e0
zhengry22 <zhengry22@mails.tsinghua.edu.cn>
2023-10-25 15:30:33 +0800

    feat(consumers): added exchange and queue

--- chat/permanent_storage.py
+++ chat/permanent_storage.py
@@ -1,6 +1,15 @@
 from consumers import Message
+from channels.db import database_sync_to_async  # 引入异步数据库操作
+from channels.generic.websocket import AsyncWebsocketConsumer
+import aio_pika
 
 
-async def perm_store(message_received: Message):
-    channel = await rabbitmq_connection.channel()
-    queue_permanent_storage = await channel.declare_queue("queue_permanent_storage")
+class PermStore:
+    def __init__(self, *args, **kwargs):
+        self.rabbitmq_connection = None
+
+    async def receive(self):
+        self.rabbitmq_connection = await aio_pika.connect_robust("amqp://localhost")
+        channel = await self.rabbitmq_connection.channel()
+        await channel.declare_exchange("storage", type="direct")
+        queue_receive = await channel.declare_queue("permanent_storage")
