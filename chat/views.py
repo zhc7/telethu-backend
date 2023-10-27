0f115f36e692fdd4fe13e34c9b0d40cfcdb3ab36
zhc7 <hc-zhang22@mails.tsinghua.edu.cn>
2023-10-27 19:34:04 +0800

    fix(chat): history should filter time earlier not later

--- chat/views.py
+++ chat/views.py
@@ -34,7 +34,7 @@ def chat_history(request):
     if t_type == 1:
         # group
         messages = MessageList.objects.filter(
-            time__gt=float(from_value), receiver=id_value, t_type=t_type
+            time__lt=float(from_value), receiver=id_value, t_type=t_type
         ).order_by("-time")[:num_value]
 
     else:
@@ -43,7 +43,7 @@ def chat_history(request):
         messages = MessageList.objects.filter(
             Q((Q(sender=id_value) & Q(receiver=user_id)))
             | Q((Q(receiver=id_value) & Q(sender=user_id))),
-            time__gt=float(from_value),
+            time__lt=float(from_value),
             t_type=t_type
         ).order_by("-time")[:num_value]
         print("messages: ", messages)
