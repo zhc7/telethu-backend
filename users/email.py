aa781fff45a51eff36a6ade594e3487b4252d5cd
zhengry22 <zhengry22@mails.tsinghua.edu.cn>
2023-11-08 22:52:34 +0800

    feat(email): verification link changed to adapt deployment

--- users/email.py
+++ users/email.py
@@ -5,7 +5,7 @@ from django.core.signing import dumps
 
 def email_sender(req, email, user_id):
     signed_data = dumps({"user_id": user_id, "email": email})
-    verification_link = req.build_absolute_uri(reverse("verify", args=[signed_data]))
+    verification_link = f"https://telethu-frontend-secoder-e8a.app.secoder.net/users/verify/{signed_data}"
     print("verification link: ", verification_link)
     subject = "Identity Authorization"
     message = f"Welcome to Telethu! To authorize your your identity, please click the link: {verification_link}"
