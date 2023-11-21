068eb0e836c0a018854a2f8932eba9c98a2ea830
zhengry22 <zhengry22@mails.tsinghua.edu.cn>
2023-11-21 23:27:08 +0800

    fix(users): delete_user doesn't need user id as param

--- users/views.py
+++ users/views.py
@@ -323,19 +323,17 @@ def user_search(req: HttpRequest):
 
 @CheckRequire
 @csrf_exempt
-def delete_user(req: HttpRequest, id):
-    # First, judge whether the user_id is logged in.
-    if req.user_id != id:
-        return request_failed(2, "User currently hasn't logged in! ", status_code=401)
-    
+def delete_user(req: HttpRequest):
+    session = SessionData(req)
+    if session.user_id is None:
+        return request_failed(2, "User isn't logging in! ", status_code=401)
     if req.method != "DELETE":
         return BAD_METHOD
-    user = User.objects.get(id=id)
+    user = User.objects.get(id=session.user_id)
     # exception
     if user is None:
         return request_failed(2, "Deleting a user that doesn't exist!", status_code=401)
     else:
-        session = SessionData(req)
         session.user_id = None
         user.userEmail = user.userEmail + "is_deleted"
         user.is_deleted = True
