2f82d392ea2e3bba40d4d640aa2a405653280071
zhengry22 <zhengry22@mails.tsinghua.edu.cn>
2023-11-30 13:14:22 +0800

    feat(chat): filter deleted and mark recalled messages while getting history

--- chat/views.py
+++ chat/views.py
@@ -20,29 +20,30 @@ def room(request, room_name):
 
 
 def chat_history(request):
-
+    print("You're getting chat history!")
     # Parameters
     from_value = int(request.GET.get("from", 0)) # Get all the message from this time 
-    to_value = int(request.GET.get("to", -1)) # Get all the message before this time, default to be -1 to show no limits
     num_value = int(request.GET.get("num", -1)) # Number of messages we ought to get, default to be -1 to show no limits
-    id_value = int(request.GET.get("id", "")) 
+    id_value = int(request.GET.get("id", "")) # id_value stands for the user that receives the message
     t_type = int(request.GET.get("t_type", ""))
     user_id = int(request.user_id)
-
-    m = MessageList.objects.filter()
-
+    print("user_id is: ", user_id)
     if t_type == 1:
         # group
+        # we should exclude the message recalled as well as the message deleted by the receiver
         messages = MessageList.objects.filter(
-            time__lt=from_value, receiver=id_value, t_type=t_type
+            ~Q(deleted_users__in=[user_id]),
+            time__lt=from_value, receiver=id_value, t_type=t_type 
         ).order_by("-time")[:num_value]
 
     else:
         print("0!")
         # user
+        # we should exclude the message recalled as well as the message deleted by the receiver
         messages = MessageList.objects.filter(
             Q((Q(sender=id_value) & Q(receiver=user_id)))
             | Q((Q(receiver=id_value) & Q(sender=user_id))),
+            ~Q(deleted_users__in=[user_id]),
             time__lt=from_value,
             t_type=t_type,
         ).order_by("-time")[:num_value]
@@ -60,9 +61,14 @@ def chat_history(request):
             receiver=msg.receiver,
             info=msg.info,
         )
+        if msg.status == MessageStatusType.RECALLED:
+            a.content = "This message has been recalled! "
         messages_list.append(a.model_dump())
     messages_list.reverse()
     print("ready!")
     print("messages_list: ", messages_list)
     return JsonResponse(messages_list, safe=False)
     # TODO: 利用上述字段获取数据库中数据
+    
+def filter_history():
+    pass
