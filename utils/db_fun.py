c7f92ae95a26812710bc9d9b13d352b813bde1e9
申君皓 <2022010726@secoder.net>
2023-12-19 16:39:16 +0800

    fix: another in problem

--- utils/db_fun.py
+++ utils/db_fun.py
@@ -220,8 +220,7 @@ def db_reject_candidate(group_id: int, rejected_member: int, self_user_id: int):
     if group is None:
         raise KeyError("group not exist")
     if (
-            not (group.group_owner.id == self_user_id
-            or self_user_id in group.group_admin.all())
+            not (group.group_owner.id == self_user_id or group.group_admin.filter(id=self_user_id).exists())
     ):
         raise KeyError("You are not the administrator")
     if not group.group_candidate_members.filter(id=rejected_member).exists():
